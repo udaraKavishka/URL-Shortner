9418c5bddeca82cc98baa186f3b1540aa2be81ad
Udara Nalawansa <udarakavishka13@gmail.com>
2025-08-05 23:31:12 +0530

    feat: saveUrl mapping in store services

--- store/store_service.go
+++ store/store_service.go
@@ -49,3 +49,19 @@ func InitializesStore() *StorageService {
 	return storeService
 }
 
+
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string){ 
+
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+
+
+
+}
