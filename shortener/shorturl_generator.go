6184bdd2ae476d0b5c30cd4b746f5eb9af5b08e8
Udara Nalawansa <udarakavishka13@gmail.com>
2025-08-06 23:24:44 +0530

    feat: base58 encoding function

---
+++ shortener/shorturl_generator.go
@@ -0,0 +1,27 @@
+package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/itchyny/base58-go"
+	"os"
+)
+
+func sha256OF(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write(([]byte(input)))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string{
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(encoded)
+
+}
+
