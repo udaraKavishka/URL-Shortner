308436571aa02964738dafb744a503c570d3137a
Udara Nalawansa <udarakavishka13@gmail.com>
2025-08-12 18:30:54 +0530

    feat:handle endpoints in the main func

--- main.go
+++ main.go
@@ -4,18 +4,30 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/udaraKavishka/go-url-shortener/handler"
+	"github.com/udaraKavishka/go-url-shortener/store"
 )
 
-func main(){
+func main() {
 	r := gin.Default()
-	r.GET("/", func(c * gin.Context){
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hey Go URL Shortener !",
 		})
 	})
 
+	r.POST("/create-shorturl", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializesStore()
+
 	err := r.Run(":9808")
-	if err != nil{
+	if err != nil {
 		panic(fmt.Sprintf("Failed to start server: %v", err))
 	}
 }
