57daaa49dad7a459b90578f7b1c590ea44b8ce3f
Udara Nalawansa <udarakavishka13@gmail.com>
2025-08-14 00:15:49 +0530

    feat: Handle railway dynamic url

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,8 +14,27 @@ type UrlCreationRequest struct {
 	UserId  string `json:"userId" binding:"required"`
 }
 
-func CreateShortUrl(c *gin.Context) {
+// func CreateShortUrl(c *gin.Context) {
 
+// 	var creationRequest UrlCreationRequest
+
+// 	if err := c.ShouldBindJSON(&creationRequest); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl, creationRequest.UserId)
+// 	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
+
+// 	host := "http://localhost:9808/"
+// 	c.JSON(200, gin.H{
+// 		"message":  "Successfully created short url",
+// 		"shortUrl": host + shortUrl,
+// 	})
+
+// }
+
+func CreateShortUrl(c *gin.Context) {
 	var creationRequest UrlCreationRequest
 
 	if err := c.ShouldBindJSON(&creationRequest); err != nil {
@@ -25,12 +45,18 @@ func CreateShortUrl(c *gin.Context) {
 	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl, creationRequest.UserId)
 	store.SaveUrlMapping(shortUrl, creationRequest.LongUrl, creationRequest.UserId)
 
-	host := "http://localhost:9808/"
-	c.JSON(200, gin.H{
-		"message":  "Successfully created short url",
-		"shortUrl": host + shortUrl,
-	})
+	host := c.Request.Host
+	scheme := "http"
+	if c.Request.TLS != nil {
+		scheme = "https"
+	}
 
+	fullShortUrl := fmt.Sprintf("%s://%s/%s", scheme, host, shortUrl)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "Successfully created short url",
+		"shortUrl": fullShortUrl,
+	})
 }
 
 // func HandleShortUrlRedirect(c *gin.Context) {
