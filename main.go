870bdf48361cf534f679a2f5caabe0d8d8a5da08
Udara Nalawansa <udarakavishka13@gmail.com>
2025-08-13 23:50:40 +0530

    feat:add env variable port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/udaraKavishka/go-url-shortener/handler"
@@ -26,8 +27,11 @@ func main() {
 
 	store.InitializesStore()
 
-	err := r.Run(":9808")
-	if err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9808"
+	}
+	if err := r.Run(":" + port); err != nil {
 		panic(fmt.Sprintf("Failed to start server: %v", err))
 	}
 }
